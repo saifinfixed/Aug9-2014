92e54386027dd26621ed8375ad86e6333f51c253
Saiful Azam <saifinfixed@gmail.com>
2014-08-09 12:39:56 -0400

    My First Java Project.

---
+++ FirstProject/src/com/google/qa/FristMathProgram.java
@@ -0,0 +1,13 @@
+package com.google.qa;
+
+public class FristMathProgram {
+	public static void main(String [] arge){
+		int fristnumber = 5;
+		int secondnumber = 10;
+		int total;
+		total = fristnumber+secondnumber;
+		
+		System.out.println(total);
+	}
+
+}
---
+++ FirstProject/src/com/google/test/ClassTwo.java
@@ -0,0 +1,10 @@
+package com.google.test;
+
+public class ClassTwo {
+	public static void main(String[] args) {
+		System.out.print("Infix Tech");
+		System.out.println("Infix Technology");
+		System.out.print("Infix Tech");
+	}
+
+}
